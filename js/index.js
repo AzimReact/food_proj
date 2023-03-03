087e3c1b176595ca7df0b8cc7e0b0724017c0f37
Azim <azimreact@gmail.com>
2023-03-03 17:53:48 +0600

    update events with regular expressions

--- js/index.js
+++ js/index.js
@@ -419,11 +419,15 @@ window.addEventListener('DOMContentLoaded', () => {
         dots[slideIndex - 1].style.opacity = 1;
     }
 
+    function replaceWidth (width) {
+        return +width.replace(/\D/ig, '');
+    }
+
     next.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)){
+        if (offset == replaceWidth(width) * (slides.length - 1)){
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += replaceWidth(width);
         };
 
         slidesField.style.transform = `translateX(-${offset}px)`
@@ -446,9 +450,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prev.addEventListener('click', () => {
         if (offset == 0){
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1)
+            offset = replaceWidth(width) * (slides.length - 1)
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= replaceWidth(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`
@@ -474,7 +478,7 @@ window.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
             slideIndex = slideTo;
             
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1)
+            offset = replaceWidth(width) * (slideTo - 1)
             slidesField.style.transform = `translateX(-${offset}px)`
 
             if (slides.length < 10) {
